401dcdbdfa8e40619c612e43777fc96992388fd9
Albert Alan-Reys <alanreys@mail.ru>
2024-12-03 10:22:06 +1000

    Improve the architecture

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 FRED_API_KEY = "ваш_ключ"
+
+fetch_info = {
+    'start_time': '2000-01-01',
+    'end_time': '2025-01-01',
+}
--- starter.py
+++ starter.py
@@ -1,12 +1,7 @@
 import logging
 
+from config import fetch_info
 from src.delegator import Delegator
-
-
-fetch_info = {
-    'start_time': '2000-01-01',
-    'end_time': '2025-01-01',
-}
 
 
 def main() -> None:
